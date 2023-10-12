a5b6efa240ab1ef96cecbc0e90627cc599417f84
csudharshanraju964 <csudharshanraju964@gmail.com>
2023-10-12 22:14:53 +0530

    Added files to repo

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseChart expenses={filteredExpenses}/>
+        <ExpenseList items={filteredExpenses} />
+      </Card>
+    </li>
+  );
+};
+
+export default Expenses;
